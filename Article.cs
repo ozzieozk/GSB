50c21eb79365aae00926ed2f6decdaad1526eb88
elucas <elucas@S-204-11>
2019-09-17 13:56:45 +0200

    Commit création de la classe article

---
+++ Article.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_
+{
+    class Article
+    {
+        private int id;
+        private int quantite;
+        private string nom;
+
+
+        public Article(int leId, int laQuantite, string leNom)
+        {
+            this.id = leId;
+            this.quantite = laQuantite;
+            this.nom = leNom;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public int Quantite { get => quantite; set => quantite = value; }
+        public string Nom { get => nom; set => nom = value; }
+    }
+}
