aef8002d30e9f3b1b1cbdf6ad3209f2601bfd78f
elucas <elucas@S-204-11>
2019-09-17 14:02:44 +0200

    commit création de la classe commande

---
+++ Commande.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_
+{
+    class Commande
+    {
+        private int id;
+        private DateTime date;
+        private string heure;
+
+
+        public Commande (int leId, DateTime laDate, string lHeure)
+        {
+            this.id = leId;
+            this.date = laDate;
+            this.heure = lHeure;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string Heure { get => heure; set => heure = value; }
+    }
+}
