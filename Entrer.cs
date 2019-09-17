918d6ae04e4a5dbeadb76826afe1c4e58a5a3470
Ozkok Ozan <oozkok@S-204-10>
2019-09-17 15:13:23 +0200

    Entrer les frere

---
+++ Entrer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GSB_
+{
+    class Entrer
+    {
+        private int id;
+        private DateTime date;
+        private string heure;
+        private int quantite;
+
+        public Entrer(int leId, DateTime laDate,  string lHeure, int laQuantite)
+        {
+            this.id = leId;
+            this.date = laDate;
+            this.heure = lHeure;
+            this.quantite = laQuantite;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public DateTime Date { get => date; set => date = value; }
+        public string Heure { get => heure; set => heure = value; }
+        public int Quantite { get => quantite; set => quantite = value; }
+    }
+}
