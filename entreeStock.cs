f9b745e42be9b70ddeea14756703c3ff38d567f8
PFISTER Mathias <matpfi@outlook.fr>
2019-09-17 14:54:14 +0200

    hyughyghèy

---
+++ entreeStock.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GSB_
+{
+	class entreeStock
+	{
+        private int id;
+        private DateTime date;
+        private string heure;
+        private int quantite;
+
+        public entreeStock(int unID, DateTime uneDate, string uneHeure, int uneQuantite)
+        {
+            this.id = unID;
+            this.date = uneDate;
+            this.heure = uneHeure;
+            this.quantite = uneQuantite;
+        }
+	}
+}
