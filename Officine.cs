e66f245a879f43849abe980498eb02c867957c60
Ozkok Ozan <oozkok@S-204-10>
2019-09-17 14:24:29 +0200

    Commit Officine sf

--- Officine.cs
+++ Officine.cs
@@ -14,20 +14,17 @@ namespace GSB_
         private string telephone;
         private string nom;
 
-        public Officine(int leId, string leCodepostal, string laVille, string leTelephone, string leNom)
-        {
-            this.Id = leId;
-            this.Codepostal = leCodepostal;
-            this.Ville = laVille;
-            this.Telephone = leTelephone;
-            this.Nom = leNom;
+        public Officine(int leId, string leCodepostal, string laVille, string leTelephone, string leNom){
+            this.id = leId;
+            this.codepostal = leCodepostal;
+            this.ville = laVille;
+            this.telephone = leTelephone;
+            this.nom = leNom;
         }
         public int Id { get => id; set => id = value; }
         public string Codepostal { get => codepostal; set => codepostal = value; }
         public string Ville { get => ville; set => ville = value; }
         public string Telephone { get => telephone; set => telephone = value; }
         public string Nom { get => nom; set => nom = value; }
-
-
     }
 }
